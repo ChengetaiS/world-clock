df16ace04c7f2b0e7e345b3bca705ad66ac551b9
ChengetaiS <162143946+ChengetaiS@users.noreply.github.com>
2025-10-05 21:16:00 +0200

    Added functionality of timers on homepage using js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+function updateTime() {
+  let harareDateElement = document.querySelector("#harare .date");
+  harareDateElement.innerHTML = moment()
+    .tz("Africa/Harare")
+    .format("dddd, DD MMMM, YYYY");
+  let harareTimeElement = document.querySelector("#harare .time");
+  harareTimeElement.innerHTML = moment()
+    .tz("Africa/Harare")
+    .format("hh:mm:ss [<small>]A[</small>]");
 
+  let berlinDateElement = document.querySelector("#berlin .date");
+  berlinDateElement.innerHTML = moment()
+    .tz("Europe/Berlin")
+    .format("dddd, DD MMMM, YYYY");
+  let berlinTimeElement = document.querySelector("#berlin .time");
+  berlinTimeElement.innerHTML = moment()
+    .tz("Europe/Berlin")
+    .format("hh:mm:ss [<small>]A[</small>]");
+
+  let newyorkDateElement = document.querySelector("#new-york .date");
+  newyorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("dddd, DD MMMM, YYYY");
+  let newyorkTimeElement = document.querySelector("#new-york .time");
+  newyorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
